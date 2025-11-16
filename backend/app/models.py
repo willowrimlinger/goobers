c6529fb429d367db42e728868fc7d7fd483f7928
Gibril <gabrielhwgo1@gmail.com>
2025-11-16 00:15:56 -0500

    refactor and adventure

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,8 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from sqlalchemy import Text
 from app import db
+import random
+from datetime import datetime, timedelta
 
 class Fingerprint(db.Model):
     __tablename__ = 'fingerprints'
@@ -10,6 +12,10 @@ class Fingerprint(db.Model):
     id: so.Mapped[int] = db.mapped_column(db.Integer, primary_key=True)
     fingerprint: so.Mapped[str] = db.mapped_column(Text, index=True, unique=True)
 
+    @classmethod
+    def get_by_fingerprint(cls, fingerprint: str):
+        return db.session.scalar(sa.select(cls).where(cls.fingerprint == fingerprint))
+    
     def __repr__(self):
         return f'<Fingerprint {self.fingerprint}>'
 
@@ -21,6 +27,36 @@ class Goober(db.Model):
     fingerprint_id: so.Mapped[int] = db.mapped_column(db.Integer, db.ForeignKey('fingerprints.id'))
 
     fingerprint: so.Mapped[Fingerprint] = db.relationship('Fingerprint', backref='goobers')
+
+    @classmethod
+    def get_by_fingerprint(cls, fingerprint: Fingerprint):
+        return db.session.scalar(sa.select(cls).where(Goober.fingerprint_id == fingerprint.id))
+    
+    @classmethod
+    def get_all(cls):
+        return db.session.scalars(sa.select(cls)).all()
+    
+    def to_json(self):
+        goober_history = GooberHistory.get_by_fingerprint(self.id)
+        return {
+            'name': self.name,
+            'fingerprint': self.fingerprint.fingerprint,
+            'last_seen': goober_history[0].timestamp if goober_history else None,
+            'events': [{'event': history.event.name, 'description': history.event.description} for history in goober_history],
+            'stats': [  
+                {'stat_name': history.event.stat_name, 'stat_value': history.event.value_float} if history.event.type == 'float' else 
+                {'stat_name': history.event.stat_name, 'stat_value': history.event.value_string} 
+                for history in goober_history]
+        }
+    
+    def go_on_adventure(self):
+        history = GooberHistory.get_by_fingerprint(self.id)
+
+        if not history or (datetime.now() - history[0].timestamp) > timedelta(seconds=30):
+            event: Event = Event.get_random_event()
+            db.session.add(GooberHistory(goober_id=self.id, event_id=event.id, timestamp=datetime.now()))
+            db.session.commit()
+
 
     def __repr__(self):
         return f'<Goober {self.name}>'
@@ -34,6 +70,10 @@ class CheckIn(db.Model):
 
     fingerprint: so.Mapped[Fingerprint] = db.relationship('Fingerprint', backref='checkins')
 
+    @classmethod
+    def get_latest(cls):
+        return db.session.scalars(sa.select(cls).order_by(CheckIn.timestamp.desc())).first()
+
     def __repr__(self):
         return f'<CheckIn {self.goober.name} at {self.timestamp}>'
 
@@ -43,13 +83,21 @@ class Event(db.Model):
     id: so.Mapped[int] = db.mapped_column(db.Integer, primary_key=True)
     name: so.Mapped[str] = db.mapped_column(Text)
     description: so.Mapped[str] = db.mapped_column(Text)
-    name: so.Mapped[str] = db.mapped_column(Text)
+    stat_name: so.Mapped[str] = db.mapped_column(Text)
     type: so.Mapped[str] = db.mapped_column(Text)
     value_float: so.Mapped[Optional[float]] = db.mapped_column(db.Float)
     value_string: so.Mapped[Optional[str]] = db.mapped_column(Text)
+
+    @classmethod
+    def get_all_ids(cls):
+        return db.session.scalars(sa.select(cls.id)).all()
+    
+    @classmethod
+    def get_random_event(cls):
+        return random.choice(db.session.scalars(sa.select(cls)).all())
     
     def __repr__(self):
-        return f'<Event {self.name} at {self.timestamp}>'
+        return f'<Event {self.name}>'
       
 class GooberHistory(db.Model):
     __tablename__ = 'goober_history'
@@ -61,6 +109,10 @@ class GooberHistory(db.Model):
 
     goober: so.Mapped[Goober] = db.relationship('Goober', backref='goober_history')
     event: so.Mapped[Event] = db.relationship('Event', backref='goober_history')
+
+    @classmethod
+    def get_by_fingerprint(cls, goober_id: int):
+        return db.session.scalars(sa.select(cls).where(cls.goober_id == goober_id).order_by(GooberHistory.timestamp.desc())).all()
 
     def __repr__(self):
         return f'<GooberHistory {self.goober.name} - {self.event.name} at {self.timestamp}>'
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,6 +4,7 @@ from flask import request, jsonify
 from app import db
 from datetime import datetime, timedelta
 import hashlib
+import json
 
 
 @version_blueprint.route('/hello')
@@ -12,16 +13,24 @@ def index():
 
 @version_blueprint.route('/goobers', methods=['GET'])
 def get_goobers():
-    goobers = Goober.query.all()
+    goobers = Goober.get_all()
     return {'goobers': [{'name': goober.name, 'fingerprint': goober.fingerprint.fingerprint} for goober in goobers]}
 
+@DeprecationWarning
 @version_blueprint.route('/goobers/<string:fingerprint>', methods=['GET'])
-def get_goober_by_fingerprint(fingerprint):
-    goober = Goober.query.join(Fingerprint).filter_by(fingerprint=fingerprint).first()
-    if goober:
-        return {'name': goober.name, 'fingerprint': goober.fingerprint.fingerprint}
-    else:
+def get_goober_by_fingerprint(fingerprint: str):
+    goober = Goober.get_by_fingerprint(Fingerprint.get_by_fingerprint(fingerprint))
+    if not goober:
         return jsonify({'error': 'Goober not found'}), 404
+    
+    history = GooberHistory.get_by_fingerprint(goober_id=goober.id)
+
+    if not history or (datetime.now() - history[0].timestamp) > timedelta(days=6):
+        event: Event = Event.get_random_event()
+        db.session.add(GooberHistory(goober_id=goober.id, event_id=event.id, timestamp=datetime.now()))
+        db.session.commit()
+
+    return jsonify(goober.to_json()), 200
 
 @version_blueprint.route('/goobers', methods=['POST'])
 def create_goober():
@@ -29,14 +38,12 @@ def create_goober():
     name: str = data.get('name')
     fingerprint: str = data.get('fingerprint')
 
-
     if not name or not fingerprint:
         return jsonify({'error': 'Name and fingerprint are required'}), 400
 
     existing_goober = Goober.query.join(Fingerprint).filter_by(fingerprint=fingerprint).first()
     if existing_goober:
         return jsonify({'error': 'Goober with this fingerprint already exists'}), 400
-    
     
     new_goober: Goober = Goober(name=name, fingerprint=Fingerprint.query.filter_by(fingerprint=fingerprint).first())
     db.session.add(new_goober)
@@ -46,16 +53,18 @@ def create_goober():
 
 @version_blueprint.route('/sessions', methods=['GET'])
 def get_latest_session():
-    checkin = CheckIn.query.order_by(CheckIn.timestamp.desc()).first()
+    checkin = CheckIn.get_latest()
+    
     if (not checkin or ((datetime.now() -checkin.timestamp) > timedelta(minutes=5))):
         return jsonify({'error': 'No recent sessions found'}), 404
     
-    goober = Goober.query.filter_by(fingerprint_id=checkin.fingerprint_id).join(Fingerprint).first()
+    goober = Goober.get_by_fingerprint(checkin.fingerprint)
     if (goober is None):
         hash = hashlib.sha256((str(checkin.fingerprint) + str(checkin.timestamp)).encode('utf-8')).hexdigest()
         return jsonify({'url': f"https://goober.garden?access_token={hash}"}), 200
     else:
-        return jsonify({'id': f"{goober.fingerprint.fingerprint}"}), 200
+        goober.go_on_adventure()
+        return goober.to_json(), 200
 
 
 @version_blueprint.route('/sessions', methods=['POST'])
@@ -77,5 +86,26 @@ def check_in_fingerprint():
     db.session.commit()
 
     return jsonify({'message': 'Check-in successful', 'fingerprint': existing_fingerprint.fingerprint}), 201
+
+@version_blueprint.route('/events', methods=['POST'])
+def create_event():
+    data = request.get_json()
+    name: str = data.get('name')
+    description: str = data.get('description')
+    stat_name: str = data.get('stat_name')
+    type: str = data.get('type')
+    if type == 'str':
+        valuestring: str = data.get('value_string')
+    if type == 'float':
+        valuefloat: float = data.get('value_float')
+
+    if not name or not description:
+        return jsonify({'error': 'Name, description, and timestamp are required'}), 400
+
+    new_event: Event = Event(name=name, description=description, stat_name=stat_name, type=type, value_string=valuestring if type == 'str' else None, value_float=valuefloat if type == 'float' else None)
+    db.session.add(new_event)
+    db.session.commit()
+
+    return jsonify({'message': 'Event created successfully', 'event': {'name': new_event.name, 'description': new_event.description}}), 201
 
 app.register_blueprint(version_blueprint, url_prefix='/v1')
